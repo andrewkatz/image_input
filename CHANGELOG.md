99580fac002e0b80ac01004eb7f15e645ddcc25e
Andrew Katz <andrew.katz@hey.com>
2020-11-02 11:44:39 -0500

    Support direct upload

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.0.5] - 2020-11-02
+### Added
+- Enable direct upload by default.
+
 ## [0.0.4] - 2020-11-02
 ### Added
 - Limit file types on file field, defaults to PNG, JPEG, and GIF.
--- lib/image_input/helpers.rb
+++ lib/image_input/helpers.rb
@@ -2,13 +2,14 @@
 
 module ImageInput
   module Helpers
-    def image_input(form, field, accept: %i(png jpeg gif))
+    def image_input(form, field, accept: %i(png jpeg gif), direct_upload: true)
       render(
         'image_input/input',
         form: form,
         field: field,
         blob: form.object.public_send(field),
-        accept: accept.map { |a| Mime[a].to_s }.join(',')
+        accept: accept.map { |a| Mime[a].to_s }.join(','),
+        direct_upload: direct_upload
       )
     end
   end
