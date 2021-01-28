c85710f7b7a801576de686721f90522c19ba1f04
Andrew Katz <andrew.katz@hey.com>
2021-01-28 09:35:45 -0500

    Customizable CSS classes

--- lib/image_input/helpers.rb
+++ lib/image_input/helpers.rb
@@ -2,14 +2,25 @@
 
 module ImageInput
   module Helpers
-    def image_input(form, field, accept: %i(png jpeg gif), direct_upload: true)
+    def image_input(
+      form,
+      field,
+      accept: %i(png jpeg gif),
+      direct_upload: true,
+      button_class: nil,
+      image_class: nil,
+      **options
+    )
       render(
         'image_input/input',
         form: form,
         field: field,
         blob: form.object.public_send(field),
         accept: accept.map { |a| Mime[a].to_s }.join(','),
-        direct_upload: direct_upload
+        direct_upload: direct_upload,
+        button_class: button_class,
+        image_class: image_class,
+        **options
       )
     end
   end
